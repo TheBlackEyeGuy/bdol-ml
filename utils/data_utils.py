59f3318b457950bb4eea2c3e6600bae48ab70025
Rohan Verma <rohanverma2004@gmail.com>
2017-11-14 05:16:05 +0530

    Added create_minibatches function
    Added this function for those who follow your tutorial at: http://briandolhansky.com/blog/2014/10/30/artificial-neural-networks-matrix-form-part-5
    
    Got this from: https://github.com/eitack/varia/blob/fa3156ae0523732fe0ad28baedb7d010a3752739/data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -99,3 +99,34 @@ def cross_validation_folds(n, k=5):
         train_ind[i] = train
 
     return train_ind, val_ind
+
+
+def label_to_bit_vector(labels, nbits):
+    bv = np.zeros((labels.shape[0], nbits))
+    for i in range(labels.shape[0]):
+        bv[i, labels[i]] = 1.0
+
+    return bv
+
+def create_minibatches(data, labels, batch_size, create_bit_vector=False):
+    N = data.shape[0]
+    if N % batch_size != 0:
+        print "Warning in create_minibatches(): Batch size {0} does not " \
+              "evenly divide the number of examples {1}.".format(batch_size,
+                                                                 N)
+    chunked_data = []
+    chunked_labels = []
+    idx = 0
+    while idx + batch_size <= N:
+        chunked_data.append(data[idx:idx+batch_size, :])
+        if not create_bit_vector:
+            chunked_labels.append(labels[idx:idx+batch_size])
+        else:
+            bv = label_to_bit_vector(labels[idx:idx+batch_size], 10)
+            chunked_labels.append(bv)
+
+        idx += batch_size
+
+    return chunked_data, chunked_labels
+
+
